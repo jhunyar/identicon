dd4bac4fc310a6e245bc861658980c3a1b14a550
James Hunyar <jrhunyar@gmail.com>
2019-05-23 23:25:47 -0400

    Add primary functions to Identicon and add image struct

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,35 @@
 defmodule Identicon do
-  @moduledoc """
-  Documentation for Identicon.
-  """
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+    |> build_grid
+  end
 
-  @doc """
-  Hello world.
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
 
-  ## Examples
+    %Identicon.Image{image | grid: grid}
+  end
 
-      iex> Identicon.hello()
-      :world
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
+  end
 
-  """
-  def hello do
-    :world
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+    
+    %Identicon.Image{hex: hex}
   end
 end
